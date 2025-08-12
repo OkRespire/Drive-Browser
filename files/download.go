f21c989dc41f7f47ef69c07400debeba20275479
OkRespire <rohane04@gmail.com>
2025-08-12 14:05:51 +0100

    Added functionality to scroll through the file list, and also able to download the files. Updated UI too.

--- files/download.go
+++ files/download.go
@@ -4,20 +4,42 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"os"
 
 	"google.golang.org/api/drive/v3"
 )
 
+var mimeTypes = map[string]string{
+	"application/vnd.google-apps.document":     "application/pdf",
+	"application/vnd.google-apps.spreadsheet":  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+	"application/vnd.google-apps.presentation": "application/pdf",
+	"application/vnd.google-apps.drawing":      "image/png",
+}
+
 func DownloadFile(srv *drive.Service, id string) {
-	resp, err := srv.Files.Get(id).Download()
+	dFile, err := srv.Files.Get(id).Fields("name, mimeType").Do()
 	if err != nil {
-		fmt.Println(err.Error())
+		log.Fatal(err.Error())
+	}
+	var resp *http.Response
+	if k, v := mimeTypes[dFile.MimeType]; v {
+		resp, err = srv.Files.Export(id, k).Download()
+
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+	} else {
+		resp, err = srv.Files.Get(id).Download()
+
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
 	}
 	defer resp.Body.Close()
-
-	dFile, err := srv.Files.Get(id).Fields("name").Do()
-	if err != nil {
+	if err := os.MkdirAll("output", 0755); err != nil {
 		log.Fatal(err.Error())
 	}
 
--- tui/renderer.go
+++ tui/renderer.go
@@ -2,11 +2,13 @@ package tui
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"drivebrowser/files"
 
 	tea "github.com/charmbracelet/bubbletea"
+	lipgloss "github.com/charmbracelet/lipgloss"
 	"google.golang.org/api/drive/v3"
 )
 
@@ -14,7 +16,8 @@ type gModel struct {
 	breadcrumb []string
 	files      []*drive.File
 	cursor     int
-	user       string
+	user       *drive.User
+	srv        *drive.Service
 }
 
 func InitialModel(ctx context.Context, srv *drive.Service, folderId string) gModel {
@@ -24,7 +27,7 @@ func InitialModel(ctx context.Context, srv *drive.Service, folderId string) gMod
 		log.Fatal(err.Error())
 	}
 
-	user_name, err := srv.About.Get().Fields("user(displayName)").Context(ctx).Do()
+	user_name, err := srv.About.Get().Fields("user(displayName, emailAddress)").Context(ctx).Do()
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -33,7 +36,8 @@ func InitialModel(ctx context.Context, srv *drive.Service, folderId string) gMod
 		breadcrumb: breadcrumbVal,
 		files:      file_list,
 		cursor:     0,
-		user:       user_name.User.DisplayName,
+		user:       user_name.User,
+		srv:        srv,
 	}
 }
 
@@ -51,10 +55,17 @@ func (m gModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, tea.Quit
 
 		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
+			m.cursor--
+			if m.cursor < 0 {
+				m.cursor = len(m.files) - 1
 			}
-
+		case "down", "j":
+			m.cursor++
+			if m.cursor > len(m.files)-1 {
+				m.cursor = 0
+			}
+		case "enter":
+			files.DownloadFile(m.srv, m.files[m.cursor].Id)
 		}
 	}
 
@@ -62,15 +73,40 @@ func (m gModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m gModel) View() string {
-	s := m.user + "\n"
+
+	breadcrumbStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#FFFFFF")).
+		Border(lipgloss.NormalBorder(), true).
+		Padding(0, 1)
+
+	contentStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#9AA0A6")).
+		Border(lipgloss.NormalBorder(), true).
+		Padding(0, 1).Bold(true)
+	breadcrumb_string := fmt.Sprintf("%s (%s)\n", m.user.DisplayName, m.user.EmailAddress)
 
 	for _, v := range m.breadcrumb {
-		s += v
+		breadcrumb_string += v
 	}
 
-	for _, f := range m.files {
-		s += "\n" + f.Name
+	file_string := ""
+
+	for i, f := range m.files {
+
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">"
+		}
+
+		file_string += fmt.Sprintf("\n%s %s", cursor, f.Name)
 	}
 
-	return s
+	// Layout
+	breadcrumbBar := breadcrumbStyle.Render(breadcrumb_string)
+	content := contentStyle.Render(file_string)
+
+	return lipgloss.JoinVertical(lipgloss.Center,
+		breadcrumbBar,
+		content,
+	)
 }
